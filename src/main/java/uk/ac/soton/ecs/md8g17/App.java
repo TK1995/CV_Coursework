61460f12722a7a981e7931eb5c47a6711b11b807
tangke <kt3n17@soton.ac.uk>
2020-12-18 16:39:59 +0000

    Second Change

--- src/main/java/uk/ac/soton/ecs/md8g17/App.java
+++ src/main/java/uk/ac/soton/ecs/md8g17/App.java
@@ -1,7 +1,10 @@
 package uk.ac.soton.ecs.md8g17;
 
+import java.util.Map.Entry;
+
 import org.apache.commons.vfs2.FileSystemException;
 import org.openimaj.data.dataset.VFSGroupDataset;
+import org.openimaj.data.dataset.VFSListDataset;
 import org.openimaj.image.DisplayUtilities;
 import org.openimaj.image.FImage;
 import org.openimaj.image.ImageUtilities;
@@ -12,14 +15,20 @@ import org.openimaj.image.ImageUtilities;
  */
 public class App {
     public static void main( String[] args ) throws FileSystemException {
-    	
-        VFSGroupDataset<FImage> groupedScenes = new VFSGroupDataset<FImage>(
-        	    "/Users/maggie/Downloads/training", ImageUtilities.FIMAGE_READER);
-        
-        System.out.println(groupedScenes.size());
-        
-        FImage randomImage = groupedScenes.getRandomInstance();
-        DisplayUtilities.display(randomImage);
+    	/*
+		 * VFSListDataset<FImage> scenes = new
+		 * VFSListDataset<FImage>("/Users/tangke/Desktop/data/training",
+		 * ImageUtilities.FIMAGE_READER); System.out.print(scenes.size());
+		 * 
+		 * DisplayUtilities.display("ATT Scenes", scenes);
+		 */
+
+		VFSGroupDataset<FImage> groupedScenes = new VFSGroupDataset<FImage>("/Users/tangke/Desktop/data/training",
+				ImageUtilities.FIMAGE_READER);
+
+		for (final Entry<String, VFSListDataset<FImage>> entry : groupedScenes.entrySet()) {
+			DisplayUtilities.display(entry.getKey(), entry.getValue());
+		}
         
     }
 }
